c31eb733a10c620d3b003cfd28b9ee3dc15bd84b
Yuki Kaito <ykaito21@gmail.com>
2018-05-24 13:18:59 +0900

    build cocktail mvc

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,46 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+end
